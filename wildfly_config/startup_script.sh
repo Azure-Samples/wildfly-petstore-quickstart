4912b49e7a8bba0012996c95773900f47aacb34a
Jason Freeberg <jr.freeberg@comcast.net>
2018-11-07 16:37:13 -0800

    Made config files more readable

--- wildfly_config/startup_script.sh
+++ wildfly_config/startup_script.sh
@@ -1,3 +1,3 @@
-# This file is executed when your app is started. In the portal, create an environment variable named 'INIT_SCRIPT'
-# with the value as the location of this file on the App Service instance (/home/site/deployments/tools/startup.sh)
+# This file is executed when your app is started. In the portal, set the "Startup Script" field 
+# as the location of this file on the App Service instance (/home/site/deployments/tools/startup_script.sh).
 /opt/jboss/wildfly/bin/jboss-cli.sh -c --file=/home/site/deployments/tools/jboss_cli_commands.cli
